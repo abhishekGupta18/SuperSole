b5fc12d55faf9fd32da93fffd1f749d7268ad74e
Guptaabhii <112747867+Guptaabhii@users.noreply.github.com>
2023-05-16 20:25:42 +0530

    refactor: made changes in footer

--- src/Component/Footer/Footer.js
+++ src/Component/Footer/Footer.js
@@ -1,69 +1,56 @@
 import "./Footer.css";
 
-import GitHubIcon from "@mui/icons-material/GitHub";
 import InstagramIcon from "@mui/icons-material/Instagram";
-import LinkedInIcon from "@mui/icons-material/LinkedIn";
 import TwitterIcon from "@mui/icons-material/Twitter";
-import MailOutlineIcon from "@mui/icons-material/MailOutline";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import GitHubIcon from "@mui/icons-material/GitHub";
 
 export const Footer = () => {
   return (
-    <footer>
+    <div className="footer">
       <div className="footer_section">
-        <div className="footer_sub_section">
-          <ul>
-            <li>
-              <strong>ABOUT COMPANY </strong>
-            </li>
-            <li>About us </li>
-            <li>Contact us </li>
-            <li>Press </li>
-            <li>Blog </li>
-          </ul>
+        <div className="social_links">
+          <h3>CONNECT WITH ME</h3>
+          <div className="social_btn">
+            <button>
+              <InstagramIcon />
+            </button>
+            <button>
+              <TwitterIcon />
+            </button>
+            <button>
+              <LinkedInIcon />
+            </button>
+            <button>
+              <GitHubIcon />
+            </button>
+          </div>
         </div>
-        <div className="footer_sub_section">
-          <ul>
-            <li>
-              <strong>ONLINE SHOPPING </strong>
-            </li>
-            <li>New Arrivals </li>
-            <li>Men </li>
-            <li>Women </li>
-            <li>Kids </li>
-          </ul>
+        <div className="tag_line">
+          <h2>SuperSole</h2>
+          <p>The essence of perfect shoes.</p>
         </div>
-        <div className="footer_sub_section">
+        <div className="policies">
+          <h3>POLICIES</h3>
           <ul>
-            <li>
-              <strong>POLICIES </strong>
-            </li>
-            <li>Return Policy </li>
+            <li>Terms & Conditions</li>
+            <li>Shipping & Return Policy</li>
             <li>Privacy Policy</li>
-            <li>Terms & Conditions </li>
-            <li>Shiping Information </li>
-          </ul>
-        </div>
-        <div className="footer_sub_section">
-          <ul>
-            <li>
-              <strong>Customer Services </strong>
-            </li>
-            <li>FAQ</li>
-            <li>Track Your Order</li>
-            <li>Return Request</li>
           </ul>
         </div>
       </div>
-      <div className="social_links">
-        <h3>Created By @abhishekGupta</h3>
-        <div className="links">
-          <GitHubIcon />
-          <InstagramIcon />
-          <LinkedInIcon />
-          <TwitterIcon />
-          <MailOutlineIcon />
-        </div>
-      </div>
-    </footer>
+      <p
+        style={{
+          fontWeight: "700",
+          display: "flex",
+          justifyContent: "center",
+          padding: "1rem",
+          alignItems: "center",
+        }}
+      >
+        Copyright ©2023 All rights reserved | This template is made with by{" "}
+        <span style={{ color: "#2563eb" }}>Abhishek Gupta❤️</span>
+      </p>
+    </div>
   );
 };
